ac30488596e5d2f2d932387c4f63e8a93a9d7a58
Your Name <you@example.com>
2026-07-23 13:53:48 -0300

    chore: setup Prisma configuration for direct and accelerate connections

--- lib/prisma.ts
+++ lib/prisma.ts
@@ -2,7 +2,7 @@ import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
 const prismaClientSingleton = () => {
-  const adapter = new PrismaPg({ connectionString: process.env.DATABASE_URL as string });
+  const adapter = new PrismaPg({ connectionString: process.env.DIRECT_URL as string });
   return new PrismaClient({ adapter });
 };
 
